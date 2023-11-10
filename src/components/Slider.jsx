151f7145a2dc344d12fcbb5ac766c658630d38ed
wevapp <raymondestrella1993@gmail.com>
2023-11-10 17:15:41 +0800

    re-create Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 // Import Swiper React components
-import 'swiper/swiper-bundle.min.css';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/swiper-bundle.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, EffectFlip, EffectFade, EffectCube, EffectCoverflow, EffectCards, EffectCreative } from 'swiper/modules';
 
 const Slider = () => {
   return (
     <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectFlip, EffectFade, EffectCube, EffectCoverflow, EffectCards, EffectCreative]}
+      spaceBetween={0}
+      effect="fade"
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
     >
-        <SwiperSlide><img src="image/d1.jpg" alt="" style={{width: '100%'}}/></SwiperSlide>
-        <SwiperSlide><img src="image/d2.jpg" alt="" style={{width: '100%'}}/></SwiperSlide>
-        <SwiperSlide><img src="image/d3.jpg" alt="" style={{width: '100%'}}/></SwiperSlide>
-        <SwiperSlide><img src="image/d4.jpg" alt="" style={{width: '100%'}}/></SwiperSlide>
+      <SwiperSlide><img src="/public/image/d1.jpg" alt="" className='object-cover w-full h-screen'/></SwiperSlide>
+      <SwiperSlide><img src="/public/image/d2.jpg" alt="" className='object-cover w-full h-screen'/></SwiperSlide>
+      <SwiperSlide><img src="/public/image/d3.jpg" alt="" className='object-cover w-full h-screen'/></SwiperSlide>
+      <SwiperSlide><img src="/public/image/d4.jpg" alt="" className='object-cover w-full h-screen'/></SwiperSlide>
     </Swiper>
   )
 }
